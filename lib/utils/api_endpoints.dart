126ee054c210bbdb478a02adf4ec35e49ce9c9e5
Abdelrahman Ashraf <102326709+Booodaaa@users.noreply.github.com>
2024-05-31 23:56:47 +0300

    Delete lib directory

--- lib/utils/api_endpoints.dart
+++
@@ -1,9 +0,0 @@
-class ApiEndPoints {
-  static const String baseUrl = 'https://api-x-robot.future-developers.cloud/api/';
-  static _AuthEndPoints authEndpoints = _AuthEndPoints();
-}
-
-class _AuthEndPoints {
-  final String registerEmail = 'auth/register';
-  final String loginEmail = 'auth/login';
-}
