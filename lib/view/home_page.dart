879fa3c6c08edcbda944f211f51ef9d4d9c3eea4
3mar2 <https://github.com/3mar2>
2024-05-31 21:58:40 +0300

    f

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../controller/home_controller.dart';
-import '../widgets/app_colors.dart';
+// import '../widgets/app_colors.dart';
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
